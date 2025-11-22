056f5c6d594a3bd985f9a5c0016f02f222ee1c6f
dominics4n <dominics4n@gmail.com>
2025-11-22 02:17:15 -0600

    Estilo super cool siono

--- bd.js
+++ bd.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 // configuracion de mi DB
+
 const db = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'node_crud',
-    port:'3306'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DB,
+    port: process.env.DB_PORT
 });
 
 
